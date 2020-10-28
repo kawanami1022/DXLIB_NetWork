27ecd14b0237e07a1134dd80c73931fce67414a1
koshiro <kawanamisos@gmail.com>
2020-10-28 09:00:16 +0900

    Edit5

--- Project1/NetWork/NetWorkState.cpp
+++ Project1/NetWork/NetWorkState.cpp
@@ -13,10 +13,14 @@
 
 NetWorkState::NetWorkState()
 {
-	//unsigned int a = UINT_MAX;
-	//std::cout << std::bitset<8*4>(a) << std::endl;
-	//a = a >> sizeof(char)*8;
-	//std::cout << std::bitset<8*4>(a) << std::endl;
+	unsigned int a =0x00000000;
+	std::cout << std::bitset<8*4>(a) << std::endl;
+	a += 0x0A;
+	a <<= sizeof(char)*8;
+	std::cout << std::bitset<8 * 4>(a) << std::endl;
+	a <<= sizeof(char) * 8;
+	std::cout << std::bitset<8 * 4>(a) << std::endl;
+
 
 	mesData_.type = MesType::TMX_SIZE;
 	active_ = ActiveState::Non;
@@ -116,39 +120,39 @@ void NetWorkState::SendMessageData()
 	std::cout << "これからデータを送信します" << std::endl;
 ;
 	mesData_.type = MesType::TMX_SIZE;
-	mesData_.data[0] = std::filesystem::file_size("map.tmx");
+	auto fileSize = std::filesystem::file_size("map.tmx");
+	mesData_.data[0] = fileSize;
 	std::cout << "送信するファイルサイズ" << mesData_.data[0] << std::endl;
 	NetWorkSend(netHandle, &mesData_, sizeof(mesData_));
 
 	mesData_.type = MesType::INIT;
 	mesData_.data[0] = 0;
+
+
 	while (!File.eof())
 	{
 		std::getline(File, lineData_);
-		for (int idx = 0; idx < lineData_.length(); idx++)
+		for (auto str:lineData_)
 		{
-			mesData_.data[1] = lineData_.data()[idx];
-			std::cout << num << ":" << static_cast<char>(mesData_.data[1]);
-			NetWorkSend(netHandle, &mesData_, sizeof(mesData_));
+			mesData_.data[1] <<= 8;
+			mesData_.data[1] = str;
 			mesData_.data[0]++;
+			std::cout << num << ":" << static_cast<char>(mesData_.data[1]);
+
 			if (mesData_.data[1] == '\r')
 			{
-				mesData_.data[1] = 0x0A;
+				mesData_.data[1] <<= 8;
+				mesData_.data[1] += 0x0A;
 				mesData_.data[0]++;
+			}
+			if ((mesData_.data[0] % 4) == 0)
+			{
 				NetWorkSend(netHandle, &mesData_, sizeof(mesData_));
 			}
-			if (File.eof())
-			{
-				std::cout <<"終端のデータ:"<< mesData_.data[1] << std::endl;
-				break;
-			}
 
+			if (mesData_.data[0] >= fileSize) { break; }
 		}
-		if (File.eof())
-		{
-			std::cout << "終端のデータ:" << mesData_.data[1] << std::endl;
-			break;
-		}
+		if (mesData_.data[0] >= fileSize) { break; }
 		std::cout << std::endl;
 	}
 	std::cout <<"計測時間:"<< timer_->IntervalMesurement() << std::endl;
@@ -166,14 +170,20 @@ void NetWorkState::ReservMessageData()
 	revdata_.resize(mesData_.data[0]);
 
 	mesData_.type = MesType::INIT;
-	auto Size = GetNetWorkSendDataLength(netHandle);
 
-	while (GetNetWorkDataLength(netHandle) > 0)
+	// ネットワークバッファのデータサイズが0になったら終了
+	while (GetNetWorkDataLength(netHandle) >= 0)
 	{
 		NetWorkRecv(netHandle, &mesData_, sizeof(mesData_));
 
-		revdata_.push_back(static_cast<char>(mesData_.data[1]));
-		std::cout << "[" << revdata_.back() << "]";
+		for (int idx = 0; idx < 4; idx++)
+		{
+			auto revData = mesData_.data[0] & 0x00ffffff;
+			revData >>= 8 * 3;
+			mesData_.data[0] <<= 8 * 1;
+			revdata_.push_back(static_cast<char>(revData));
+			std::cout << "[" << revdata_.back() << "]";
+		}
 		if(revdata_.back() =='\n')	std::cout << std::endl;
 	}
 	std::cout << "取得したデータサイズ:" <<"          "<< sizeof(revdata_)<< "byte"<<std::endl;
